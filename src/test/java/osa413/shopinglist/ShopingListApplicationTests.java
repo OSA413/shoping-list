713518575aac6e71421b32b44a07dd1b31245b7a
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2023-12-12 19:17:32 +0500

    Initial commit

---
+++ src/test/java/osa413/shopinglist/ShopingListApplicationTests.java
@@ -0,0 +1,13 @@
+package osa413.shopinglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopingListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
